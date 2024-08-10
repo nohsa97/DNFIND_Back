a2834470b68aac632d336f1873a17532512adb11
noh <nohsa97@nate.com>
2024-08-11 04:21:45 +0900

    Feat : 유저 장비 정보 전송 추가

--- index.ts
+++ index.ts
@@ -10,15 +10,15 @@ const dot = dotenv.config();
 
 app.use(cors());
 
+//캐릭터 찾기
 app.get("/findUser", function (req, res) {
-  const test = req.query;
   const { server } = req.query;
   const { characterName } = req.query;
 
   const apiKey: string | undefined = process.env.API_KEY;
 
   const BASEURL =
-    `https://api.neople.co.kr/df/servers/${server}/characters?characterName=${characterName}&wordType=full&apikey=` +
+    `https://api.neople.co.kr/df/servers/${server}/characters?characterName=${characterName}&limit=28&wordType=full&apikey=` +
     apiKey;
 
   axios
@@ -28,6 +28,33 @@ app.get("/findUser", function (req, res) {
       console.log(`송신 완료. ${server} ${characterName} `);
       console.log("전송");
       res.send(Data);
+    })
+    .catch((error) => {
+      res.send("잘못된 접근입니다." + error);
+      throw error;
+    });
+});
+
+//유저 인포 (아이템)
+app.get("/userInfo", function (req, res) {
+  const { server } = req.query;
+  const { characterId } = req.query;
+
+  const apiKey: string | undefined = process.env.API_KEY;
+
+  const BASEURL =
+    `https://api.neople.co.kr/df/servers/${server}/characters/${characterId}/equip/equipment?apikey=` +
+    apiKey;
+
+  axios
+    .get(BASEURL)
+    .then((result) => {
+      // const Data: UserData[] = result.data.rows;
+      console.log(`송신 완료. ${server} ${characterId} `);
+      console.log("전송");
+
+      // console.log(result);
+      res.send(result.data);
     })
     .catch((error) => {
       res.send("잘못된 접근입니다." + error);
